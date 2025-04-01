550e58089c1d178ebffa7820eef97e73ff20b780
Kuldeep Kumar <upsainian3330@gmail.com>
2025-04-01 22:08:32 +0530

    ref and portals

--- src/02_TIC_TAC_TOE/components/GameBoard.jsx
+++ src/02_TIC_TAC_TOE/components/GameBoard.jsx
@@ -1,36 +1,35 @@
 import React, { useState } from 'react'
 
+const GameBoard = ({ onSelectSquare, board }) => {
+  // const[gameBoard, setGameBoard]=useState(initialGameBoard)
 
-const GameBoard = ({onSelectSquare, board}) => {
+  // function handleSelectSquare(rowIndex, colIndex){
+  //     setGameBoard((prevGameBoard)=>{
+  //         const updatedBoard=[...prevGameBoard.map(innerArray=>[...innerArray])]
+  //         updatedBoard[rowIndex][colIndex]=activeSymbolPlayer
+  //         return updatedBoard;
+  //     })
 
-    
-
-    // const[gameBoard, setGameBoard]=useState(initialGameBoard)
-
-    // function handleSelectSquare(rowIndex, colIndex){
-    //     setGameBoard((prevGameBoard)=>{
-    //         const updatedBoard=[...prevGameBoard.map(innerArray=>[...innerArray])]
-    //         updatedBoard[rowIndex][colIndex]=activeSymbolPlayer
-    //         return updatedBoard;
-    //     })
-
-    //     onSelectSquare();
-    // }
+  //     onSelectSquare();
+  // }
   return (
-    <ol id='game-board'>
-        {
-            board.map((row, rowIndex)=>(
-            <li key={rowIndex}>
-                <ol>
-                    {row.map((playerSymbol, colIndex)=>(<li key={colIndex}>
-                    <button onClick={()=>onSelectSquare(rowIndex, colIndex)}
-                     disabled={playerSymbol!==null}>
-                        {playerSymbol}
-                        </button></li>))}
-                </ol>
-            </li>))
-        }
-
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
     </ol>
   )
 }
